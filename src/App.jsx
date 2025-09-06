0734b8b68af67c81b6add0148b3700d466ed1686
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2025-09-06 12:17:59 +0600

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import AnnouncementBar from "./AccouncementBar/AnnouncementBar";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import NewsLetter from "./NewsLetter/NewsLetter";
+
+function App() {
+  return (
+    <>
+      <AnnouncementBar />
+      <Header />
+      <Main />
+      <NewsLetter />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
